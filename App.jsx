e68872265213a93d460cb98b05d249d94c285f61
Dominator646 <nikitapopow643@gmail.com>
2026-01-10 16:59:35 +0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CallbackPage from './pages/CallbackPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth/callback" element={<CallbackPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
