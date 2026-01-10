d5a90791fd7809397d2b3b2daabd86ef5853b95a
Dominator646 <nikitapopow643@gmail.com>
2026-01-10 16:24:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function login() {
-  window.location.href = "https://discord.com/oauth2/authorize?client_id=1431712036626104401&response_type=code&redirect_uri=https%3A%2F%2Fdiscord-site-backend-production.up.railway.app%2Fauth%2Fdiscord%2Fcallback&scope=identify";
+  window.location.href = "https://discord-site-backend-production.up.railway.app/auth/discord/callback";
 }
 
 const token = new URLSearchParams(window.location.search).get("token");
