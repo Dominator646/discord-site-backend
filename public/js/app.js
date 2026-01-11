6b383906e4f5a4533850ffacbc008e4838650ffe
Dominator646 <nikitapopow643@gmail.com>
2026-01-11 22:11:52 +0300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,28 @@
+async function loadUser() {
+  const res = await fetch('/api/me');
 
-fetch('/api/me')
-  .then(r => r.json())
-  .then(u => {
-    const div = document.getElementById('user');
-    div.innerHTML = `
+  if (res.status === 401) {
+    window.location.href = '/';
+    return;
+  }
+
+  const u = await res.json();
+
+  renderTopBar(u);
+}
+
+function renderTopBar(u) {
+  const div = document.getElementById('user');
+  div.innerHTML = `
+    <div class="avatar-wrapper" onclick="toggleUserMenu()">
       <img src="https://cdn.discordapp.com/avatars/${u.discord_id}/${u.avatar}.png" />
-      <span>${u.username}</span>
-      <b>💰 ${u.coins}</b>
-    `;
-  });
+      <span class="coins">💰 ${u.coins}</span>
+    </div>
+
+    <div id="userMenu" class="user-menu hidden">
+      <button onclick="openProfile()">Профиль</button>
+    </div>
+  `;
+}
+
+loadUser();
